31d57ba51fcaace5338228cd3184dc58d85adf2c
MarimiOshi <marimi.oshi@gmail.com>
2025-05-25 22:34:26 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // config.js
 // 重要： "YOUR_API_KEY" を実際のGemini APIキーに置き換えてください。
 // このファイルを公開リポジトリにコミットしないでください。
-const GEMINI_API_KEY = "AIzaSyBZwpiy8rhg6uvIPl9I4T3AVfCaJcqgdOQ";
+const GEMINI_API_KEY = "AIzaSyAaURIyA0MkHK18IUmBUwq2g66280jknMs";
